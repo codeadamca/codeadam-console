08b97ba92b2cde7c04ca897c6c26f085debf29ed
Adam Thomas <thomasadam83@hotmail.com>
2022-01-22 16:43:27 -0500

    Converted user views to use blade views

--- resources/views/users/add.blade.php
+++ resources/views/users/add.blade.php
@@ -12,45 +12,15 @@
 
         @csrf
 
-        <div class="w3-margin-bottom">
-            <label for="first">First Name:</label>
-            <input type="text" name="first" id="first" value="{{old('first')}}" required class="w3-input w3-border">
-            
-            @error ('first')
-                <small class="w3-text-red">{{$message}}</small>
-            @enderror
-        </div>
+        @include ('layout.forms.text', ['name' => 'first', 'label' => 'First Name'])
 
-        <div class="w3-margin-bottom">
-            <label for="last">Last Name:</label>
-            <input type="text" name="last" id="last" value="{{old('last')}}" required class="w3-input w3-border">
+        @include ('layout.forms.text', ['name' => 'last', 'label' => 'Last Name'])
 
-            @error ('last')
-                <small class="w3-text-red">{{$message}}</small>
-            @enderror
-        </div>
+        @include ('layout.forms.text', ['name' => 'email', 'type' => 'email'])
 
-        <div class="w3-margin-bottom">
-            <label for="email">Email:</label>
-            <input type="email" name="email" id="email" value="{{old('email')}}" required class="w3-input w3-border">
+        @include ('layout.forms.text', ['name' => 'password', 'type' => 'password'])
 
-            @error ('email')
-                <small class="w3-text-red">{{$message}}</small>
-            @enderror
-        </div>
-
-        <div class="w3-margin-bottom">
-            <label for="password">Password:</label>
-            <input type="password" name="password" id="password" class="w3-input w3-border">
-
-            @error ('password')
-                <small class="w3-text-red">{{$message}}</small>
-            @enderror
-        </div>
-
-        <div class="w3-center">
-            <button type="submit" class="w3-button w3-green">Add User</button>
-        </div>
+        @include ('layout.forms.button', ['label' => 'Add User'])
 
     </form>
 
