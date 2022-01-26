10974b06541d713a720c956bf0e9b84916c0ab83
Adam Thomas <thomasadam83@hotmail.com>
2022-01-26 16:30:17 -0500

    Added DS_Store to .gitignore

--- database/migrations/2022_01_21_192135_create_meme_tag_table.php
+++ database/migrations/2022_01_21_192135_create_meme_tag_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateTagTable extends Migration
+class CreateMemeTagTable extends Migration
 {
     /**
      * Run the migrations.
