3765ab8d126415118169d81af25d6ebfd9331a24
Adam Thomas <thomasadam83@hotmail.com>
2022-03-21 10:51:17 -0400

    Adjusted spacing for checkboxes

--- resources/views/layout/forms/checkbox.blade.php
+++ resources/views/layout/forms/checkbox.blade.php
@@ -6,7 +6,7 @@
 
         <div class="w3-container w3-border w3-padding">
             @foreach($options as $option)
-                <label class="w3-quarter">
+                <label class="w3-third">
                     <input type="checkbox" name="{{$name}}[]" value="{{$option->id}}" class="w3-check" 
                         {{(is_array(old($name, $selected ?? false)) && in_array($option->id,old($name, $selected ?? false))) ? 'checked' : ''}}>
                     {{$option->title}}
