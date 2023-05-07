405f208621b839a11de2abafe2e76b1ab61fb6bc
Adam <thomasadam83@hotmail.com>
2023-05-07 11:13:11 -0400

    Added list view

--- resources/views/journals/list.blade.php
+++ resources/views/journals/list.blade.php
@@ -4,14 +4,14 @@
 
 <section class="w3-padding ca-container-large">
 
-    @include ('layout.title', ['title' => 'Manage Articles'])
+    @include ('layout.title', ['title' => 'Manage Journals'])
 
     <div>
         <div class="w3-twothird">
-            @include ('layout.breadcrumbs', ['title' => 'Manage Articles'])
+            @include ('layout.breadcrumbs', ['title' => 'Manage Journals'])
         </div>
         <div class="w3-third w3-right-align w3-small ">
-            <a href="/articles/types/list">Manage Article Types</a>
+            <a href="/topics/list">Manage Topics</a>
         </div>
     </div>
 
@@ -19,62 +19,51 @@
         <tr class="w3-dark-grey">
             <th class="ca-col-icon"></th>
             <th class="ca-col-image"></th>
-            <th>Title</th>
-            <th>Type</th>
+            <th>Name</th>
+            <th>Topics</th>
             <th class="ca-col-icon"></th>
             <th class="ca-col-icon"></th>
             <th class="ca-col-icon"></th>
         </tr>
-        <?php foreach($articles as $article): ?>
+        <?php foreach($journals as $journal): ?>
             <tr>
                 <td>
-                    {{$article->id}}
+                    {{$journal->id}}
                 </td> 
                 <td>
-                    @if ($article->image)
+                    @if ($journal->image)
                         <div class="w3-center w3-light-grey w3-padding w3-border">
-                            <img src="{{asset('storage/'.$article->image)}}" width="50">
+                            <img src="{{asset('storage/'.$journal->image)}}" width="50">
                         </div>
                     @endif
                 </td>
                 <td>
-                    {{$article->title}}
+                    {{$journal->name}}
                     <small>
+                        {{$journal->description}}
                         <br>
-                        {{date('F jS, Y', strtotime($article->published_at))}}
+                        {{date('F jS, Y', strtotime($journal->published_at))}}
                         <br>
-                        @if ($article->instagram_id)
-                            <i class="fab fa-instagram"></i>
-                        @endif
-                        @if ($article->twitter_id)
-                            <i class="fab fa-twitter"></i>
-                        @endif
-                        @if ($article->soundcloud_id)
-                            <i class="fab fa-soundcloud"></i>
-                        @endif
-                        @if ($article->home == 'Yes')
-                            <i class="fas fa-home"></i>
-                        @endif                        
-                        @if ($article->url)
-                            <a href="{{$article->url}}"><i class="fas fa-link"></i></a>
+                        @if ($journal->url)
+                            <a href="{{$journal->url}}"><i class="fas fa-link"></i></a>
                         @endif
                     </small>
                 </td>
                 <td>
-                    {{$article->type->title}}
+                    
                 </td>
                 <td>
-                    <a href="/articles/image/{{$article->id}}">
+                    <a href="/journals/image/{{$journal->id}}">
                         <i class="fas fa-camera"></i> 
                     </a>
                 </td>
                 <td>
-                    <a href="/articles/edit/{{$article->id}}">
+                    <a href="/journals/edit/{{$journal->id}}">
                         <i class="fas fa-edit"></i>
                     </a>
                 </td>
                 <td>
-                    <a href="/articles/delete/{{$article->id}}">
+                    <a href="/journals/delete/{{$journal->id}}">
                         <i class="fas fa-trash-alt mute"></i>
                     </a>
                 </td>
@@ -82,7 +71,7 @@
         <?php endforeach; ?>
     </table>
 
-    @include ('layout.forms.button', ['label' => 'Add Article', 'href' => '/articles/add'])
+    @include ('layout.forms.button', ['label' => 'Add Journal', 'href' => '/journals/add'])
 
 </section>
 
